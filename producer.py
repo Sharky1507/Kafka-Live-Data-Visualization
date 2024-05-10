d68d97a2a9ca71194a7f0c1030af4e00345b2343
Kafeel <101697894+Sharky1507@users.noreply.github.com>
2024-05-10 21:59:55 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,12 +1,14 @@
 from kafka import KafkaProducer
 import time
 import psutil
-topic_name = 'test-topic'
-kafka_server = 'localhost:9092'
+topic_name = '<write your topic name here>'
+kafka_server = '<write your server here>'
+#setup server
 producer = KafkaProducer(bootstrap_servers=kafka_server)
 for i in range(1000):
      time.sleep(0.5)
      cpu_percentage = int(psutil.cpu_percent())
      print(cpu_percentage)
+     # producer.send expects input in bytes
      cpu_percentage_bytes = cpu_percentage.to_bytes(2, 'big')
      producer.send(topic_name, cpu_percentage_bytes)
